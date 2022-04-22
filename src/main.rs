914465b22ce32bc5c4f6db1b8e4e4a77e159e2d5
Shaleen Jain <shaleen@jain.sh>
2022-04-22 16:41:42 +0000

    update to clap v3

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,42 @@ use anyhow::Result;
 use std::fs;
 use std::io;
 use std::io::{Read, Write};
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{Command, Arg};
 
 use mates::cli;
 use mates::utils;
 
 fn main() -> Result<()> {
-    let app = App::new("mates")
+    let app = Command::new("mates")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Markus Unterwaditzer")
         .about("A simple commandline addressbook")
-        .setting(AppSettings::SubcommandRequired)
-        .subcommand(SubCommand::with_name("index").about("Rewrite/create the index"))
+        .subcommand_required(true)
+        .subcommand(Command::new("index").about("Rewrite/create the index"))
         .subcommand(
-            SubCommand::with_name("mutt-query")
+            Command::new("mutt-query")
                 .about("Search for contact, output is usable for mutt's query_command.")
-                .arg(Arg::with_name("disable-empty-line").long("disable-empty-line").help("Disable printing an empty first line"))
-                .arg(Arg::with_name("query").required(true)),
+                .arg(Arg::new("disable-empty-line").long("disable-empty-line").help("Disable printing an empty first line"))
+                .arg(Arg::new("query").required(true)),
         )
         .subcommand(
-            SubCommand::with_name("file-query")
+            Command::new("file-query")
                 .about("Search for contact, return just the filename.")
-                .arg(Arg::with_name("query").required(true)),
+                .arg(Arg::new("query").required(true)),
         )
         .subcommand(
-            SubCommand::with_name("email-query")
+            Command::new("email-query")
                 .about("Search for contact, return \"name <email>\".")
-                .arg(Arg::with_name("query").required(true)),
+                .arg(Arg::new("query").required(true)),
         )
         .subcommand(
-            SubCommand::with_name("add")
+            Command::new("add")
                 .about("Take mail from stdin, add sender to contacts. Print filename."),
         )
         .subcommand(
-            SubCommand::with_name("edit")
+            Command::new("edit")
                 .about("Open contact (given by filepath or search-string) interactively.")
-                .arg(Arg::with_name("file-or-query").required(true)),
+                .arg(Arg::new("file-or-query").required(true)),
         )
         .get_matches();
 
@@ -51,29 +51,29 @@ fn main() -> Result<()> {
     };
 
     match app.subcommand() {
-        ("index", Some(_args)) => {
+        Some(("index", _)) => {
             println!(
                 "Rebuilding index file \"{}\"...",
                 config.index_path.display()
             );
             cli::build_index(&config.index_path, &config.vdir_path)?;
         }
-        ("mutt-query", Some(args)) => {
+        Some(("mutt-query", args)) => {
             if let Some(value) = args.value_of("query") {
                 cli::mutt_query(&config, args.is_present("disable-empty-line"), value)?
             }
         }
-        ("file-query", Some(args)) => {
+        Some(("file-query", args)) => {
             if let Some(value) = args.value_of("query") {
                 cli::file_query(&config, value)?
             }
         }
-        ("email-query", Some(args)) => {
+        Some(("email-query", args)) => {
             if let Some(value) = args.value_of("query") {
                 cli::email_query(&config, value)?
             }
         }
-        ("add", Some(..)) => {
+        Some(("add", _)) => {
             let stdin = io::stdin();
             let mut email = String::new();
             stdin.lock().read_to_string(&mut email)?;
@@ -88,7 +88,7 @@ fn main() -> Result<()> {
             let index_entry = utils::index_item_from_contact(&contact)?;
             index_fp.write_all(index_entry.as_bytes())?;
         }
-        ("edit", Some(args)) => {
+        Some(("edit", args)) => {
             if let Some(value) = args.value_of("file-or-query") {
                 cli::edit_contact(&config, value)?
             }
